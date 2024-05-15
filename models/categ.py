02c11ed296758a0791b6b941bdfa2822a3a91f7e
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2024-05-15 17:32:38 +0000

    Se eliminaron archivos innecesarios

--- models/categ.py
+++
@@ -1,7 +0,0 @@
-from odoo import tools,fields, models, api, _
-from odoo.exceptions import ValidationError
-
-class ProductCategory(models.Model):
-    _inherit = 'product.category'
-
-    valor_hora = fields.Float('Valor Hora')
--- models/sale_order_grupo.py
+++
@@ -1,9 +0,0 @@
-from odoo import tools,fields, models, api, _
-from odoo.exceptions import ValidationError
-
-class SaleOrderGrupo(models.Model):
-    _name = 'sale.order.grupo'
-
-    order_id = fields.Many2one('sale.order')
-    grupo = fields.Char('Grupo')
-    amount = fields.Float('Monto')
